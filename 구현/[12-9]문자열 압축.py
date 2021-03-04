ca31cfe9942d71b40de1cf072c2e5fd5bef085eb
JoonKimLang <khjohny95@yonsei.ac.kr>
2021-03-04 12:20:32 +0000

    12-9 changed

--- 구현/[12-9]문자열 압축.py
+++ 구현/[12-9]문자열 압축.py
@@ -1,45 +1,34 @@
-from queue import Queue
-
 def solution(s):
-    curShortestLen = 1001
     
-    k = 1
-    queue = Queue()
-    while k <= len(s):
-        for i in range(0,len(s),k):
-            queue.put(s[i:i+k])
-        prevTempStr = ""  
-        tempShortestLen = 0
-        tempRep = False
-        tempRepNum = 0
-        while queue.empty() == False :
-            tempStr = queue.get().rstrip()
-
-            #앞의 섭스트링과 반복이 아닐경우
-            if tempStr != prevTempStr:
-                tempShortestLen += len(tempStr)
-                prevTempStr = tempStr
-                tempRepNum = 1
-                tempRep = False
-            #앞에 섭스트링과 반복일 경우
+    answer = 1001
+    
+    for i in range(1, len(s)): 
+        curIdx = 0
+        tempRst = 0
+        curLen = 1
+        while curIdx < len(s):
+            #새로운게 잡히는 경우
+            if curLen == 1:
+                if s[curIdx:curIdx+i] == s[curIdx+i:curIdx+(i*2)]:
+                    curLen += 1
+                    tempRst+= i
+                    curIdx +=i
+                else:
+                    tempRst += len(s[curIdx:curIdx+i].rstrip())
+                    curIdx += i
+            #연속된 값이 잡혔던 경우
             else:
-                #반복횟수가 겹치면 안되기 때문에
-                tempRepNum+=1
-                if tempRepNum == 10:
-                    tempShortestLen+=1
-                elif tempRepNum == 100:
-                    tempShortestLen+=1
-                elif tempRepNum == 1000:
-                    tempShortestLen+=1
-                if tempRep == False:
-                    tempShortestLen+=1
-                    tempRep = True
-        print(tempShortestLen)
-        if tempShortestLen < curShortestLen:
-            curShortestLen = tempShortestLen
-        k+=1
-                
-                
-            
-            
-    return curShortestLen
+                if s[curIdx:curIdx+i] == s[curIdx+i:curIdx+(i*2)]:
+                    curLen +=1
+                    curIdx +=i
+                else:                    
+                    tempRst += len(str(curLen))
+                    curLen = 1
+                    curIdx+=i                
+        
+        answer = min(answer, tempRst)
+        
+    if len(s) == 1:
+        answer = 1
+        
+    return answer
